1c3615411806dbce3fba268875b90817ff3995cf
vincentherrmann <vincent.herrmann@web.de>
2019-02-19 11:02:45 +0100

    add mac option

--- tests/test_network.py
+++ tests/test_network.py
@@ -4,11 +4,11 @@ from network_graph import *
 class TestNetwork(TestCase):
     def test_network(self):
         net = Network()
-        net.add_layer('input_layer', [2, 12])
-        net.add_layer('hidden_layer', [4, 10])
+        net.add_layer('input_layer', [5, 12])
+        net.add_layer('hidden_layer', [7, 12])
         net.add_layer('output_layer', [1, 16])
 
-        net.add_conv1d_connections('input_layer', 'hidden_layer', kernel_size=3)
+        net.add_conv1d_connections('input_layer', 'hidden_layer', kernel_size=3, padding=(1, 1))
         net.add_full_connections('hidden_layer', 'output_layer')
         assert net.num_units == 80
 
@@ -20,11 +20,11 @@ class TestNetworkForceLayout(TestCase):
     def test_simulation(self):
         net = Network()
         net.add_layer('input_layer', [2, 12])
-        net.add_layer('hidden_layer_1', [8, 10])
+        net.add_layer('hidden_layer_1', [8, 12])
         net.add_layer('hidden_layer_2', [8, 8])
-        net.add_layer('output_layer', [1, 16])
+        net.add_layer('output_layer', [1, 10])
 
-        net.add_conv1d_connections('input_layer', 'hidden_layer_1', kernel_size=3)
+        net.add_conv1d_connections('input_layer', 'hidden_layer_1', kernel_size=3, padding=(1, 1))
         #net.add_full_connections('hidden_layer', 'output_layer')
         net.add_conv1d_connections('hidden_layer_1', 'hidden_layer_2', kernel_size=3)
         net.add_full_connections('hidden_layer_2', 'output_layer')
@@ -33,16 +33,25 @@ class TestNetworkForceLayout(TestCase):
         #                         'hidden_layer_1': ['hidden_layer_2', 'output_layer'],
         #                         'hidden_layer_2': ['output_layer']}
 
-        layout = NetworkForceLayout(net, attraction=0.01, normalize_attraction=True, gravity=-0.005)
-        input_positions = torch.linspace(-1.5, 1.5, 12)
-        input_positions = input_positions.repeat([2, 1])
-        input_positions = torch.stack([input_positions, torch.zeros([2, 12])-2.], dim=2)
-        input_positions[1, :, 1] -= 0.2
-        layout.set_position(net.layers['input_layer'], input_positions.view(-1, 2), fix=True)
+        layout = NetworkForceLayout(net,
+                                    attraction=0.005,
+                                    normalize_attraction=False,
+                                    gravity=-0.005,
+                                    step_size=0.5,
+                                    centering=0.0,
+                                    friction=1.,
+                                    noise=0.01,
+                                    mac=0.7)
+        #input_positions = torch.linspace(-1.5, 1.5, 12)
+        #input_positions = input_positions.repeat([2, 1])
+        #input_positions = torch.stack([input_positions, torch.zeros([2, 12])-2.], dim=2)
+        #input_positions[1, :, 1] -= 0.2
+        #layout.set_position(net.layers['input_layer'], input_positions.view(-1, 2), fix=False)
 
-        output_positions = torch.linspace(-2, 2, 16).unsqueeze(0)
-        output_positions = torch.stack([output_positions, torch.zeros([1, 16]) + 2.], dim=2)
-        layout.set_position(net.layers['output_layer'], output_positions.view(-1, 2), fix=False)
+        #output_positions = torch.linspace(-2, 2, 10).unsqueeze(0)
+        #output_positions = torch.stack([output_positions, torch.zeros([1, 10]) + 2.], dim=2)
+        #layout.set_position(net.layers['output_layer'], output_positions.view(-1, 2), fix=False)
+        layout.simulation_step()
         line_data = layout.line_data()
 
         #layout.plot()
@@ -51,5 +60,22 @@ class TestNetworkForceLayout(TestCase):
         #layout.plot()
         ani = animate_simulation(layout)
         plt.show()
+
+
+class TestBarnesHutSimulation(TestCase):
+    def test_barnes_hut_simulation(self):
+        net = Network()
+        net.add_layer('input_layer', [1, 111])
+        layout = NetworkForceLayout(net,
+                                    attraction=0.0,
+                                    normalize_attraction=True,
+                                    gravity=-0.005,
+                                    step_size=0.1,
+                                    centering=0.0,
+                                    friction=1.,
+                                    noise=0.0,
+                                    use_barnes_hut=True)
+        layout.simulation_step()
+
 
 
--- tests/test_quadTree.py
+++ tests/test_quadTree.py
@@ -10,28 +10,43 @@ class TestQuadTree(TestCase):
         tic = time.time()
         qt = QuadTree(positions, mass)
         qt.traverse(positions, mass)
+        print("levels:", qt.levels)
         print("duration:", time.time() - tic)
+        print("")
 
         positions = torch.rand(1000, 2)
         mass = torch.ones(1000)
         tic = time.time()
         qt = QuadTree(positions, mass)
         qt.traverse(positions, mass)
+        print("levels:", qt.levels)
         print("duration:", time.time() - tic)
+        print("")
 
         positions = torch.rand(10000, 2)
         mass = torch.ones(10000)
         tic = time.time()
         qt = QuadTree(positions, mass)
         qt.traverse(positions, mass)
+        print("levels:", qt.levels)
         print("duration:", time.time() - tic)
+        print("")
 
-        positions = torch.rand(100000, 2)
-        mass = torch.ones(100000)
+        positions = torch.rand(30000, 2)
+        mass = torch.ones(30000)
         tic = time.time()
         qt = QuadTree(positions, mass)
         qt.traverse(positions, mass)
+        print("levels:", qt.levels)
         print("duration:", time.time() - tic)
+        print("")
+
+        big_tensor = torch.rand(30000, 2)
+        tic = time.time()
+        diff = big_tensor.unsqueeze(0) - big_tensor.unsqueeze(1)
+        dist = torch.norm(diff, 2, dim=2)
+        print("brute force time 30000:", time.time()-tic)
+
 
         #duration: 0.007807016372680664
         #duration: 0.03351593017578125
