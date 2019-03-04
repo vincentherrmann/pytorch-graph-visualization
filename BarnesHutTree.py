561860ec85bd83068d99f93885b6275c6172a01c
vincentherrmann <vincent.herrmann@web.de>
2019-03-04 17:17:00 +0100

    remove unnecessary indexing rows

--- BarnesHutTree.py
+++ BarnesHutTree.py
@@ -23,9 +23,10 @@ def energy_function(m1, m2, difference, distance):
 class BarnesHutTree(object):
     def __init__(self, pos, mass, max_levels=100, device='cpu'):
         super().__init__()
-        self.levels = 0
-        self.max_levels = max_levels
         self.device = device
+
+        self.num_levels = 0
+        self.max_levels = max_levels
 
         self.num_dim = pos.shape[1]
         self.num_o = 2**self.num_dim
@@ -34,111 +35,136 @@ class BarnesHutTree(object):
         max_val = torch.max(pos) + 1e-4
         self.size = max_val - min_val
 
-        norm_pos = (pos - min_val.unsqueeze(0)) / self.size.unsqueeze(0)
+        norm_pos = (pos - min_val.unsqueeze(0)) / self.size.unsqueeze(0)  # normalized position of all points
 
-        self.quadrant_mass = []
+        # level-wise tree parameters (list index is the corresponding level)
+        self.node_mass = []
         self.center_of_mass = []
-        self.section_indexing = []
         self.is_end_node = []
-        sections = torch.zeros(pos.shape[0], dtype=torch.long, device=self.device)
-        num_sections = 1
+        self.node_indexing = []
+
+        point_nodes = torch.zeros(pos.shape[0], dtype=torch.long, device=self.device)  # node in which each point falls on the current level
+        num_nodes = 1
 
         while True:
-            self.levels += 1
-            num_divisions = 2**self.levels
+            self.num_levels += 1
+            num_divisions = 2**self.num_levels
 
-            # calculate the section in which each point falls
+            # calculate the orthant in which each point falls
             point_orthant = torch.floor(norm_pos * num_divisions).long()
-            point_orthant = torch.sum((point_orthant % 2) * (2**torch.arange(self.num_dim, device=self.device)).unsqueeze(0), dim=1)
+            point_orthant = (point_orthant % 2) * (2**torch.arange(self.num_dim, device=self.device).unsqueeze(0))
+            point_orthant = torch.sum(point_orthant, dim=1)
 
-            sections *= self.num_o
-            sections += point_orthant
+            # calculate node indices from point orthants
+            point_nodes *= self.num_o
+            point_nodes += point_orthant
 
-            # calculate total mass and center of mass for each section
-            q_mass = torch.zeros(num_sections * self.num_o, device=self.device)
-            q_mass.scatter_add_(0, sections, mass)
+            # calculate total mass of each section
+            node_mass = torch.zeros(num_nodes * self.num_o, device=self.device)
+            node_mass.scatter_add_(0, point_nodes, mass)
 
-            q_com = torch.zeros(num_sections * self.num_o, self.num_dim, device=self.device)
+            # calculate center of mass of each node
+            node_com = torch.zeros(num_nodes * self.num_o, self.num_dim, device=self.device)
             for d in range(self.num_dim):
-                q_com[:, d].scatter_add_(0, sections, pos[:, d])
-            q_com /= q_mass.unsqueeze(1)
+                node_com[:, d].scatter_add_(0, point_nodes, pos[:, d])
+            node_com /= node_mass.unsqueeze(1)
 
-            continued_points = q_mass[sections] > mass
-            end_nodes = sections[continued_points == 0]
+            # determine if node is end node
+            point_is_continued = node_mass[point_nodes] > mass  # only points that are not the only ones in their node are passed on to the next level
+            end_nodes = point_nodes[point_is_continued == 0]  # nodes with only one point are end nodes
+            is_end_node = torch.zeros(num_nodes * self.num_o, device=self.device, dtype=torch.uint8)
+            is_end_node[end_nodes] = 1
 
-            empty_node = q_mass == 0.
-            #empty_node[end_nodes] = 1
-            continued_quadrants = 1 - empty_node
-            non_empty_q = continued_quadrants.nonzero().squeeze(1)
-            new_indices = torch.arange(non_empty_q.shape[0], device=self.device)
-            section_indexing = torch.zeros(num_sections, self.num_o, dtype=torch.long, device=self.device) - 1.
-            section_indexing[non_empty_q / self.num_o, non_empty_q % self.num_o] = new_indices
-            num_sections = non_empty_q.shape[0]
+            node_is_continued = node_mass > 0.
+            non_empty_nodes = node_is_continued.nonzero().squeeze(1)  # indices of non-empty nodes
+            num_nodes = non_empty_nodes.shape[0]
 
-            #print("section_indexing_shape:", section_indexing.shape)
+            # create new node indexing: only non-empty nodes have positive indices, end nodes have the index -1
+            node_indexing = self.create_non_empty_node_indexing(non_empty_nodes, node_mass.shape[0], self.num_o)
 
-            q_end = torch.zeros_like(continued_quadrants)
-            q_end[end_nodes] = 1
-            q_end = q_end[continued_quadrants]
-            q_mass = q_mass[continued_quadrants]
-            q_com = q_com[continued_quadrants, :]
-            self.quadrant_mass.append(q_mass)
-            self.center_of_mass.append(q_com)
-            self.section_indexing.append(section_indexing)
-            self.is_end_node.append(q_end)
+            # only pass on nodes that are continued
+            is_end_node = is_end_node[node_is_continued]
+            node_mass = node_mass[node_is_continued]
+            node_com = node_com[node_is_continued, :]
 
-            sections = section_indexing[sections / self.num_o, sections % self.num_o]
+            self.node_mass.append(node_mass)
+            self.center_of_mass.append(node_com)
+            self.node_indexing.append(node_indexing)
+            self.is_end_node.append(is_end_node)
 
-            # discard points that already have their own section
-            if torch.sum(continued_points) < 1:
+            # update the node index of each point
+            point_nodes = node_indexing[point_nodes / self.num_o, point_nodes % self.num_o]
+
+            # discard points in end nodes
+            pos = pos[point_is_continued]
+            mass = mass[point_is_continued]
+            point_nodes = point_nodes[point_is_continued]
+            norm_pos = norm_pos[point_is_continued]
+
+            if torch.sum(point_is_continued) < 1:
                 break
-            if self.levels >= self.max_levels:
+            if self.num_levels >= self.max_levels:
+                num_points_in_nodes = torch.zeros_like(node_mass, dtype=torch.long)
+                num_points_in_nodes.scatter_add_(0, point_nodes, torch.ones_like(mass, dtype=torch.long))
+                max_points_in_node = torch.max(num_points_in_nodes)
+                node_indexing = self.create_non_empty_node_indexing((num_points_in_nodes > 0).nonzero().squeeze(1),
+                                                                    max_points_in_node)
                 print("too many levels!")
                 break
-            pos = pos[continued_points]
-            mass = mass[continued_points]
-            sections = sections[continued_points]
-            norm_pos = norm_pos[continued_points]
-            #print("max mass:", torch.max(q_mass))
+
+    def create_non_empty_node_indexing(self, non_empty_nodes, num_nodes, refinement_factor):
+        # create new node indexing: only non-empty nodes have positive indices, end nodes have the index -1
+        new_indices = torch.arange(non_empty_nodes.shape[0], device=self.device)
+        node_indexing = torch.zeros(num_nodes // refinement_factor, refinement_factor,
+                                    dtype=torch.long, device=self.device) - 1
+        node_indexing[non_empty_nodes // refinement_factor, non_empty_nodes % refinement_factor] = new_indices
+        return node_indexing
+
 
     def traverse(self, x, m, mac=0.7, force_function=gravity_function):
-        #print("levels:", self.levels)
         force = torch.zeros_like(x)
-        pairs_o = torch.cat([torch.arange(x.shape[0], dtype=torch.long, device=self.device).unsqueeze(1).repeat(1, self.num_o).view(-1, 1),
-                           torch.arange(self.num_o, dtype=torch.long, device=self.device).unsqueeze(1).repeat(x.shape[0], 1)], dim=1)
-        for l in range(self.levels):
-            indexing = self.section_indexing[l]
+
+        # pair each point with all nodes of the first level
+        pairs_o = torch.cat([torch.arange(x.shape[0],
+                                          dtype=torch.long,
+                                          device=self.device).unsqueeze(1).repeat(1, self.num_o).view(-1, 1),
+                             torch.arange(self.num_o,
+                                          dtype=torch.long,
+                                          device=self.device).unsqueeze(1).repeat(x.shape[0], 1)],
+                            dim=1)
+
+        for l in range(self.num_levels):
+            indexing = self.node_indexing[l]
             pairs = pairs_o.clone()
+
+            # adjust indexing of the nodes
             pairs[:, 1] = indexing[pairs_o[:, 1] / self.num_o, pairs_o[:, 1] % self.num_o]
+            # remove nodes with index -1
             pairs = pairs[pairs[:, 1] >= 0, :]
-            #print("pairs shape:", pairs.shape)
 
             this_com = self.center_of_mass[l][pairs[:, 1], :]
-            this_mass = self.quadrant_mass[l][pairs[:, 1]]
+            this_mass = self.node_mass[l][pairs[:, 1]]
 
             diff = x[pairs[:, 0], :] - this_com
             dist = torch.norm(diff, 2, dim=1)
             section_size = self.size / 2 ** (l + 1)
             d2r = section_size / dist
 
-            #if l < self.levels - 1:
             accept = d2r < mac
             end_node = self.is_end_node[l][pairs[:, 1]]
             accept = torch.max(accept, end_node) - (dist <= 1e-9)
-            #else:
-            #    accept = 1 - torch.isnan(d2r)
-
-            #print("num accepted:", torch.sum(accept).item())
 
             this_f = force_function(m1=this_mass[accept],
                                     m2=m[pairs[:, 0]][accept],
                                     difference=diff[accept],
                                     distance=dist[accept])
-            #this_f = -gravity * (this_mass[accept] * m[pairs[:, 0]][accept] / (dist[accept]**3 + 1e-5)).unsqueeze(1) * diff[accept]
             force[:, 0].scatter_add_(0, pairs[:, 0][accept], this_f[:, 0])
             force[:, 1].scatter_add_(0, pairs[:, 0][accept], this_f[:, 1])
 
+            # get pairs that were not accepted
             refine = pairs[(accept == 0).nonzero(), :].squeeze(1)
+
+            # expand the indexing of the nodes for the next level
             refine[:, 1] *= self.num_o
             refine = refine.unsqueeze(1).repeat(1, self.num_o, 1)
             refine[:, :, 1] = refine[:, :, 1] + torch.arange(self.num_o, dtype=torch.long, device=self.device).unsqueeze(0)
